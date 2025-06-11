0505e4faaafc61f9eca4df6c019eb44ca257f370
Marcus Kastner <marcus.kastner@arestravelinc.com>
2025-06-11 09:45:17 -0700

    icon path and form enable

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -21,7 +21,7 @@ const Contact = ({ id, content, title }: ContactProps) => {
       <Row justify="center" align="middle">
         <Col lg={12} md={12} sm={24} xs={24}>
           <Slide direction="up" triggerOnce>
-            <FormGroup autoComplete="off" onSubmit={handleSubmit}>
+            <FormGroup autoComplete="off" onSubmit={handleSubmit} data-netlify>
               <h6>Contact Form</h6>
               <Col span={24}>
                 <Input
